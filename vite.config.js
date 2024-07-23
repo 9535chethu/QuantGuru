5c158d92be13cf84fb0684cc5b69cba0cf989417
9535chethu <chethankr4321@gmail.com>
2024-07-23 16:39:13 +0530

    Prepare for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/QuantGuru/', // Ensure this matches your repository name
   plugins: [react()],
-});
+  base: '/QuantGuru/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
